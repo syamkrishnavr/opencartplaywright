85cb5b99dc56a01f72fd88faeef8d7599c6bcc3c
syamkrishnavr <syamkrishnavr@gmail.com>
2026-01-11 17:00:08 +0530

    first commit

---
+++ test.config.ts
@@ -0,0 +1,10 @@
+export class TestConfig{
+appUrl="https://naveenautomationlabs.com/opencart"
+//valid login credentials- create your own login account
+email="pavanol@abc.com"
+password="test@123"
+//product details
+productName="MacBook"
+productQuantity="2"
+totalPrice="$1,204.00"
+}
